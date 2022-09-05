73695be8f687389926c85cfd02242b52d9b9405e
unknown <81352231>
2022-09-05 13:40:30 -0300

    project clean

---
+++ backend/src/main/java/com/backend/vendas/Models/seller.java
@@ -0,0 +1,12 @@
+package com.backend.vendas.Models;
+
+import java.sql.Date;
+import java.util.UUID;
+
+public class seller {
+    private UUID id;
+    private String name;
+    private String Total;
+    private Date DateLocal;
+
+}
